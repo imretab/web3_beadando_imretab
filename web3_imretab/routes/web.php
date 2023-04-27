5f3bece18aaca678496d5b605a55448a73fd674f
imretab <imre.tabajdi@gmail.com>
2023-04-27 16:43:39 +0200

    updated web.php - added routes
    sign-in get/post
    edit_profile get/post
    logout

--- web3_imretab/routes/web.php
+++ web3_imretab/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('navbar');
 });
+Route::get('/sign-in',[UserController::class,'Login']);
+Route::post('/sign-in',[UserController::class,'SignIn']);
+Route::get('/sign-out',[UserController::class,'LogOut']);
+Route::get('/edit_profile',[UserController::class,'Profile'])->middleware('auth');
+Route::post('/edit_profile',[UserController::class,'EditProfile'])->middleware('auth');
