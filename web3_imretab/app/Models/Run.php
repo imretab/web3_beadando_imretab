5d606c56930b6c9e54f88e8c1457ba034956f66a
imretab <imre.tabajdi@gmail.com>
2023-04-28 01:23:44 +0200

    Created run.php - for the Run table

---
+++ web3_imretab/app/Models/Run.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Run extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'uploader',
+        'run_category',
+        'run_title',
+        'time',
+        'youtube_link',
+        'comment_onrun'
+    ];
+}
