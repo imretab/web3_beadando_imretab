5556674b7b1033ff819bc51e324254c91df4b284
imretab <imre.tabajdi@gmail.com>
2023-04-29 08:45:34 +0200

    Created RunController
    Added showcategory for showing the categories on a select form
    Added uploadrun for the createrun form

---
+++ web3_imretab/app/Http/Controllers/RunController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Run;
+use Illuminate\Support\Facades\Auth;
+class RunController extends Controller
+{
+    public function ShowCategory(){
+        $categories = Category::all();
+        return view('Runs.upload_run')->
+        with('category_options',$categories);
+    }
+    public function UploadRun(Request $request){
+        $fields = $request->validate([
+            'category'=>['required'],
+            'runTitle' => ['required'],
+            'time' => ['required'],
+            'ylink' =>['required'],
+            'commentonrun' => []
+        ]);
+        $run = Run::create([
+            'run_category' => $fields['category'],
+            'run_title' => $fields['runTitle'],
+            'time' => $fields['time'],
+            'youtube_link' => $fields['ylink'],
+            'comment_onrun' => $fields['commentonrun'],
+            'uploader' => Auth::user()->id
+        ]);
+        return redirect('/create-run');
+    }
+}
