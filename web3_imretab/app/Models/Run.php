4cb5e6448fc4e6554bf264c4b8ff67245d10ceca
imretab <imre.tabajdi@gmail.com>
2023-05-01 22:15:00 +0200

    Updated run.php
    -Added two functions to reach the runCategory and uploaderName values

--- web3_imretab/app/Models/Run.php
+++ web3_imretab/app/Models/Run.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Run extends Model
 {
@@ -22,4 +26,10 @@ class Run extends Model
         'uploaded_at',
         'isAccepted'
     ];
+    public function runCategory(): HasOne{
+        return $this->hasOne(Category::class,'id','run_category');
+    }
+    public function uploaderName() : HasOne{
+        return $this->hasOne(User::class,'id','uploader');
+    }
 }
