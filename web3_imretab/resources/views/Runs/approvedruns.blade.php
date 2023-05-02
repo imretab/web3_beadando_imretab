a8445f8b2ddf38ccff95413e76de0f48a9f37890
imretab <imre.tabajdi@gmail.com>
2023-05-02 13:34:23 +0200

    Update approvedruns.blade.php
    -Added some formatting to look like a table

--- web3_imretab/resources/views/Runs/approvedruns.blade.php
+++ web3_imretab/resources/views/Runs/approvedruns.blade.php
@@ -5,17 +5,21 @@
     <div class="text-white bg-dark p-5 border border-3 border-secondary rounded-lg">
 
         <h1 class="mb-3">All the speedruns</h1>
+        <div class="row border border-primary">
+            <div class="col border border-primary"><h4>Game</h4></div> 
+            <div class="col border border-primary"><h4>Runner</h4></div> 
+            <div class="col border border-primary"><h4>Title</h4></div> 
+            <div class="col border border-primary"><h4>Time</h4></div> 
+        </div>
             @foreach ($acceptedRuns as $runs)
-            <div>
-                <br>
-                {{$runs->run_title}}
-                {{$runs->uploaderName->name}}
-                {{$runs->runCategory->categories}}
-                <br>
-                {{$runs->time}}
+            <div class="row border border-warning">
+                <div class="col border border-warning">{{$runs->runCategory->categories}}</div>
+                <div class="col border border-warning">{{$runs->uploaderName->name}}</div>
+                <div class="col border border-warning">{{$runs->run_title}}</div>
+                <div class="col border border-warning">{{$runs->time}}</div>
             </div>
             @endforeach
-
+        
 </div>
 
 @endsection
