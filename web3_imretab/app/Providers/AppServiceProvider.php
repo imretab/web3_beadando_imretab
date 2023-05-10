3d39c0535f956e25f4689fd8e49525dc668a74c2
imretab <imre.tabajdi@gmail.com>
2023-05-10 09:32:24 +0200

    Update AppServiceProvider.php
    -Added the MainLayoutViewComposer on the boot function

--- web3_imretab/app/Providers/AppServiceProvider.php
+++ web3_imretab/app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\View;
+use App\View\Composers\MainLayoutViewComposer;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*',MainLayoutViewComposer::class);
     }
 }
