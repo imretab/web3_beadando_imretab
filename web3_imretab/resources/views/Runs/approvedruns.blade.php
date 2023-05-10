92064434560cb42d1052eaadee93987c40fd6e42
imretab <imre.tabajdi@gmail.com>
2023-05-10 09:31:50 +0200

    Update approvedruns.blade.php
    -Added link for the uploader to show their profile

--- web3_imretab/resources/views/Runs/approvedruns.blade.php
+++ web3_imretab/resources/views/Runs/approvedruns.blade.php
@@ -5,8 +5,7 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 @section('content')
 <script src="{{ url('assets/js/registration.profilepic.js') }}"></script>
-
-<div class="container d-flex align-items-center justify-content-center text-center p-5">
+<div class="container-fluid align-items-center justify-content-center text-center p-5">
     <div class="text-white bg-dark p-5 border border-3 border-secondary rounded-lg">
 
         <h1 class="mb-3">All the speedruns</h1>
@@ -26,7 +25,7 @@
                     </div>
                 </div>
                 <div class="col border-end border-primary">{{$runs->runCategory->categories}}</div>
-                <div class="col border-end border-primary">{{$runs->uploaderName->name}}</div>
+                <div class="col border-end border-primary"><a href="/profile/{{$runs->uploaderName->id}}">{{$runs->uploaderName->name}}</a></div>
                 <div class="col border-end border-primary">{{$runs->run_title}}</div>
                 <div class="col border-end border-primary">{{$runs->time}}</div>
             </div>
